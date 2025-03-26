9ff6379bcc1aebcfd0e5728fd6a026ffbf202608
jake <jake@mayan.finance>
2025-03-26 19:37:11 +0330

    Add wormhole functions

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -113,6 +113,10 @@ pub mod circle {
             }
             tokio::time::sleep(std::time::Duration::from_secs(sleep_time)).await;
             sleep_time *= 2;
+
+            if sleep_time > 60 {
+                sleep_time = 60;
+            }
         }
     }
 
@@ -134,5 +138,136 @@ pub mod circle {
             ));
         }
         Ok(circle_message_v2_response.messages.unwrap()[0].clone())
+    }
+
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct CircleMessageV1 {
+        pub attestation: Option<Bytes>,
+        pub message: Option<Bytes>,
+        pub event_nonce: Option<String>,
+    }
+
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct CircleMessageV1Response {
+        pub messages: Option<Vec<CircleMessageV1>>,
+        pub error: Option<String>,
+    }
+
+    pub async fn get_circle_message_v1(
+        domain_id: u64,
+        transaction_hash: String,
+        timeout_seconds: u64,
+    ) -> anyhow::Result<CircleMessageV1> {
+        let start_time = std::time::Instant::now();
+        let mut sleep_time = 1;
+        loop {
+            let circle_message_v1 = match _get_circle_message_v1(domain_id, transaction_hash.clone()).await {
+                Ok(circle_message_v1) => circle_message_v1,
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    tokio::time::sleep(std::time::Duration::from_secs(sleep_time)).await;
+                    sleep_time *= 2;
+                    continue;
+                }
+            };
+            let attestation_to_string = match circle_message_v1.attestation.clone() {
+                Some(attestation) => attestation.to_string(),
+                None => continue,
+            };
+            if attestation_to_string != "PENDING" && attestation_to_string.starts_with("0x") {
+                return Ok(circle_message_v1);
+            }
+            if start_time.elapsed() > std::time::Duration::from_secs(timeout_seconds) {
+                return Err(anyhow::anyhow!("Timeout"));
+            }
+            tokio::time::sleep(std::time::Duration::from_secs(sleep_time)).await;
+            sleep_time *= 2;
+
+            if sleep_time > 60 {
+                sleep_time = 60;
+            }
+        }
+    }
+
+    async fn _get_circle_message_v1(
+        domain_id: u64,
+        transaction_hash: String,
+    ) -> anyhow::Result<CircleMessageV1> {
+        let url = format!(
+            "https://iris-api.circle.com/v1/messages/{}/{}",
+            domain_id, transaction_hash
+        );
+        let response = reqwest::get(url).await?;
+        let body = response.text().await?;
+        if body.contains("PENDING") {
+            return Err(anyhow::anyhow!("Pending"));
+        }
+        let circle_message_v1_response: CircleMessageV1Response = serde_json::from_str(&body)?;
+        if circle_message_v1_response.error.is_some() {
+            return Err(anyhow::anyhow!(
+                "Error: {:?}",
+                circle_message_v1_response.error
+            ));
+        }
+        Ok(circle_message_v1_response.messages.unwrap()[0].clone())
+    }
+}
+
+pub mod wormhole {
+    use alloy::primitives::Bytes;
+    use serde::Deserialize;
+
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct WormholeVaaResponse {
+        pub data: Vec<WormholeVaa>,
+    }
+
+    #[derive(Debug, Clone, Deserialize)]
+    pub struct WormholeVaa {
+        pub vaa: Option<String>, // base64 encoded
+    }
+
+    pub async fn get_vaa(tx_hash: String, timeout_seconds: u64) -> anyhow::Result<Bytes> {
+        let start_time = std::time::Instant::now();
+        let mut sleep_time = 1;
+        loop {
+            match _get_vaa(tx_hash.clone()).await {
+                Ok(vaa) => return Ok(vaa),
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                }
+            };
+            if start_time.elapsed() > std::time::Duration::from_secs(timeout_seconds) {
+                return Err(anyhow::anyhow!("Timeout"));
+            }
+            tokio::time::sleep(std::time::Duration::from_secs(sleep_time)).await;
+            sleep_time *= 2;
+
+            if sleep_time > 60 {
+                sleep_time = 60;
+            }
+        }
+    }
+
+    async fn _get_vaa(tx_hash: String) -> anyhow::Result<Bytes> {
+        let url = format!(
+            "https://api.wormholescan.io/api/v1/vaas/?txHash={}",
+            tx_hash
+        );
+        let response = reqwest::get(url).await?;
+        let body = response.text().await?;
+        let wormhole_vaa_response: WormholeVaaResponse = serde_json::from_str(&body)?;
+        if wormhole_vaa_response.data.is_empty() {
+            return Err(anyhow::anyhow!("No VAAs found"));
+        }
+        let vaa_base64 = match &wormhole_vaa_response.data[0].vaa {
+            Some(vaa) => vaa,
+            None => return Err(anyhow::anyhow!("Vaa is not returned")),
+        };
+        if vaa_base64.is_empty() {
+            return Err(anyhow::anyhow!("Vaa is empty"));
+        }
+        let vaa = base64::decode(&vaa_base64)?;
+        Ok(vaa.try_into()?)
     }
 }
